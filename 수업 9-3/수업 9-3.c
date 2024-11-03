9029b9923df8d06a3c70c19534369153d1495cbe
김량곤 <ryan20050504@gmail.com>
2024-11-03 18:06:57 +0900

    텍스트 실습2

---
+++ 수업 9-3/수업 9-3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int getline(char* line) {
+	int ch;
+	int i = 0;
+	while ((ch = getchar()) != '\n') {
+		line[i++] = ch;
+	}
+	line[i] = '\0';
+	return i;
+}
+
+int main(void) {
+	char input[100];
+	FILE* fp_src;
+	FILE* fp_dest;
+
+	if ((fp_src = fopen("output.txt", "r")) == NULL) {
+		printf("error...");
+		return 0;
+	}
+	if ((fp_dest = fopen("output2.txt", "w")) == NULL) {
+		printf("error...");
+		return 0;
+	}
+	while (!feof(fp_src)) {
+		fgets(input, 100, fp_src);
+		printf("Ãâ·Â Áß");
+		fputs(input, fp_dest);
+	}
+
+	fclose(fp_src);
+	fclose(fp_dest);
+	return 0;
+}
